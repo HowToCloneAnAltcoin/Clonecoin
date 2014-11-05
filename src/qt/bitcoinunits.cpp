7b6a2ad68deb97caab802c460ebfe5104fa72e2c
HowToCloneAnAltcoin <admin@howtocloneanaltcoin>
2014-11-05 22:39:38 +0800

    Change LTC to CLN

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -38,9 +38,9 @@ QString BitcoinUnits::name(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("LTC");
-    case mBTC: return QString("mLTC");
-    case uBTC: return QString::fromUtf8("μLTC");
+    case BTC: return QString("CLN");
+    case mBTC: return QString("mCLN");
+    case uBTC: return QString::fromUtf8("μCLN");
     default: return QString("???");
     }
 }
